159bc734fc0dce2c74682c7c8a5488124f7746f7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DevExpress.Xpf.Grid;
+
+namespace ColumnHeaderCustomizationArea {
+
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+
+        #region #events
+        private void TableView_ShowGridMenu(object sender, GridMenuEventArgs e) {
+            // Check whether this event was raised for a column's context menu.
+            // If not - exit and leave the menu shown.
+            if (e.MenuType != GridMenuType.Column) return;
+
+            // Check whether this event was raised for a custom button.
+            // If not - exit and hide the menu.
+            if ((e.TargetElement as Control).Tag == null) {
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            // Show the context menu when a custom button is clicked.
+            view.GridMenu.ShowPopup(sender as Button);
+        }
+
+        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e) {
+            // Don't show the context menu when a custom button is right-clicked.
+            e.Handled = true;
+        }
+        #endregion #events
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to add a custom button to a column header
+
+
+<p>The following example demonstrates how to modify a column header in a grid's Table View. In this sample, it is illustrated how to add a button with an icon to a column header's customization area.</p>
+
+<br/>
+
+
