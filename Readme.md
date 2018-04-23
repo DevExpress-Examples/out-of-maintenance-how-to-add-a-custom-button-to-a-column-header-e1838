acffa87aeaf2a19ac2d39035064c307c5eb13415
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to add a custom button to a column header
+
+
+<p>The following example demonstrates how to modify a column header in a grid's Table View. In this sample, it is illustrated how to add a button with an icon to a column header's customization area.</p>
+
+
+<h3>Description</h3>
+
+<p>For this, it is necessary to define a <strong>DataTemplate</strong>, which represents a button, and assign it to the <strong>TableView.ColumnHeaderCustomizationAreaTemplate</strong> property. This template specifies a button and some of its properties. For example, it invokes the standard context menu on clicking this button (but disables invoking this menu when either the column header or the custom button within it are clicked).</p>
+
+<br/>
+
+
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+Imports DevExpress.Wpf.Grid
+
+Namespace ColumnHeaderCustomizationArea
+
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		#Region "#events"
+		Private Sub TableView_ShowGridMenu(ByVal sender As Object, ByVal e As GridMenuEventArgs)
+			' Check whether this event was raised for a column's context menu.
+			' If not - exit and leave the menu shown.
+			If e.MenuType <> GridMenuType.Column Then
+				Return
+			End If
+
+
+			' Check whether this event was raised for a custom button.
+			' If not - exit and hide the menu.
+			If (TryCast(e.TargetElement, Control)).Tag Is Nothing Then
+				e.Handled = True
+				Return
+			End If
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			' Show the context menu when a custom button is clicked.
+			view.GridMenu.ShowPopup(TryCast(sender, Button))
+		End Sub
+
+		Private Sub Button_PreviewMouseRightButtonUp(ByVal sender As Object, ByVal e As _
+		MouseButtonEventArgs)
+			' Don't show the context menu when a custom button is right-clicked.
+			e.Handled = True
+		End Sub
+		#End Region
+
+	End Class
+End Namespace
+
+
+
+
+
