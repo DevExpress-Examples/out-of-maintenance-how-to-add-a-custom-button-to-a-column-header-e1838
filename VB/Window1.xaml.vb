781520f2d521bda16e1803122fb8ccbb395571a2
DevExpressExampleBot <fakeEmail>
2022-12-16 17:43:34 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,42 +1,39 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Windows.Controls
 Imports System.Windows.Input
 Imports DevExpress.Xpf.Grid
 
 Namespace ColumnHeaderCustomizationArea
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		#Region "#events"
-		Private Sub TableView_ShowGridMenu(ByVal sender As Object, ByVal e As GridMenuEventArgs)
-			' Check whether this event was raised for a column's context menu.
-			' If not - exit and leave the menu shown.
-			If e.MenuType <> GridMenuType.Column Then
-				Return
-			End If
+'#Region "#events"
+        Private Sub TableView_ShowGridMenu(ByVal sender As Object, ByVal e As GridMenuEventArgs)
+            ' Check whether this event was raised for a column's context menu.
+            ' If not - exit and leave the menu shown.
+            If e.MenuType <> GridMenuType.Column Then Return
+            ' Check whether this event was raised for a custom button.
+            ' If not - exit and hide the menu.
+            If TryCast(e.TargetElement, Control).Tag Is Nothing Then
+                e.Handled = True
+                Return
+            End If
+        End Sub
 
-			' Check whether this event was raised for a custom button.
-			' If not - exit and hide the menu.
-			If (TryCast(e.TargetElement, Control)).Tag Is Nothing Then
-				e.Handled = True
-				Return
-			End If
-		End Sub
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Show the context menu when a custom button is clicked.
+            Me.view.GridMenu.ShowPopup(TryCast(sender, Button))
+        End Sub
 
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			' Show the context menu when a custom button is clicked.
-			view.GridMenu.ShowPopup(TryCast(sender, Button))
-		End Sub
-
-		Private Sub Button_PreviewMouseRightButtonUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-			' Don't show the context menu when a custom button is right-clicked.
-			e.Handled = True
-		End Sub
-		#End Region ' #events
-	End Class
+        Private Sub Button_PreviewMouseRightButtonUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+            ' Don't show the context menu when a custom button is right-clicked.
+            e.Handled = True
+        End Sub
+'#End Region  ' #events
+    End Class
 End Namespace
